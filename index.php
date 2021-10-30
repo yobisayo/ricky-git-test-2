c301a87cb21d504c9e73402ba08760ab048ee425
yobisayo <alvinken24@gmail.com>
2021-10-30 22:22:32 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quis ratione, corporis aperiam quas impedit, blanditiis incidunt animi tenetur esse officiis? Saepe iste magnam natus incidunt alias distinctio voluptatibus enim.</p>
 </body>
 </html>
